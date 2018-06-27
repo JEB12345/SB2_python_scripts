9e04f1fdc2de6a5adffee80de9f2218ce59b95e3
Jonathan <jonathan.bruce@nasa.gov>
2018-06-27 11:58:02 -0700

    Changed some formatting

--- Demo_Star_Packing.py
+++ Demo_Star_Packing.py
@@ -19,7 +19,7 @@ SBModuleNames = (['M' + str(i + 1) for i in xrange(numModules)])
 # Need to look into XML formatting for Hebi Gains
 # sio.loadmat('defaultGains.mat')
 
-lookup = hebi.Lookup() # Get table of all Hebi motors
+lookup = hebi.Lookup()  # Get table of all Hebi motors
 sleep(2)  # gives the Lookup process time to discover modules
 
 # Displays the Hebi modules found on the network
@@ -64,31 +64,40 @@ CmdSB = hebi.GroupCommand(GroupSB.size)
 
 ######################## Actual Code ###################################
 GroupSB.command_lifetime = 0.0
-CmdSB.position = np.ones(GroupSB.size)*0.0
+CmdSB.position = np.ones(GroupSB.size) * 0.0
 GroupSB.send_command(CmdSB)
 
 # Set Gains for the packing controller
-CmdSB.control_strategy = np.ones(GroupSB.size)*3.0
-CmdSB.position_kp = np.ones(GroupSB.size)*1
-CmdSB.effort_kp = np.ones(GroupSB.size)*0.2
-CmdSB.effort_ki = np.ones(GroupSB.size)*0.01
+CmdSB.control_strategy = np.ones(GroupSB.size) * 3.0
+CmdSB.position_kp = np.ones(GroupSB.size) * 1
+CmdSB.effort_kp = np.ones(GroupSB.size) * 0.2
+CmdSB.effort_ki = np.ones(GroupSB.size) * 0.01
 
-CmdSB.effort_max_output = np.ones(GroupSB.size)*3.0
-CmdSB.effort_min_output = np.ones(GroupSB.size)*-3.0
+CmdSB.effort_max_output = np.ones(GroupSB.size) * 3.0
+CmdSB.effort_min_output = np.ones(GroupSB.size) * -3.0
 
 GroupSB.send_command(CmdSB)
 
 # Setup the star packing set points
-SBtransform = np.array([[19,20,21,15,8,2, 14, 9, 4, 13, 7, 6, 1, 18, 12, 3, 17, 11, 5, 16, 10, 23, 24, 22],[18, 3, 13, 22, 4, 15, 7, 14, 20, 9, 17, 1, 21, 2, 11, 8, 16, 24, 10, 19, 5, 12, 23, 6]])
+SBtransform = np.array([[19, 20, 21, 15, 8, 2, 14, 9, 4, 13, 7, 6, 1, 18, 12, 3, 17, 11, 5, 16, 10, 23, 24, 22],
+                        [18, 3, 13, 22, 4, 15, 7, 14, 20, 9, 17, 1, 21, 2, 11, 8, 16, 24, 10, 19, 5, 12, 23, 6]])
 
-slope = np.array([-1.13272388059701,-1.14042158304600,-1.17338351598342,-1.06945782765140,-1.11349186621372,-1.14497930796643,-1.12547082818874,-1.08895423738307,-1.08169633323703,-1.12573961622541,-1.12234550720277,-1.12037814814265,-1.11246037551817,-1.11100024243662,-1.10440750557301,-1.13555410889649,-1.11643316697733,-1.09564556365700,-1.12041645665323,-1.13072891187365,-1.14080189055838,-1.17501817049112,-1.13024261777840,-1.10180013546606])
-offset = np.array([108.254000000000,100.192000000000,124.433000000000,101.073000000000,110.140000000000,114.641000000000,108.872000000000,109.739000000000,104.905000000000,111.072000000000,113.721000000000,106.830000000000,114.410000000000,108.207000000000,94.9220000000000,114.409000000000,109.408000000000,110.794000000000,107.684000000000,104.415000000000,105.778000000000,105.691000000000,109.936000000000,106.484000000000])+5
-#offset = [x+5 for x in offset]
+slope = np.array(
+    [-1.13272388059701, -1.14042158304600, -1.17338351598342, -1.06945782765140, -1.11349186621372, -1.14497930796643,
+     -1.12547082818874, -1.08895423738307, -1.08169633323703, -1.12573961622541, -1.12234550720277, -1.12037814814265,
+     -1.11246037551817, -1.11100024243662, -1.10440750557301, -1.13555410889649, -1.11643316697733, -1.09564556365700,
+     -1.12041645665323, -1.13072891187365, -1.14080189055838, -1.17501817049112, -1.13024261777840, -1.10180013546606])
+offset = np.array(
+    [108.254000000000, 100.192000000000, 124.433000000000, 101.073000000000, 110.140000000000, 114.641000000000,
+     108.872000000000, 109.739000000000, 104.905000000000, 111.072000000000, 113.721000000000, 106.830000000000,
+     114.410000000000, 108.207000000000, 94.9220000000000, 114.409000000000, 109.408000000000, 110.794000000000,
+     107.684000000000, 104.415000000000, 105.778000000000, 105.691000000000, 109.936000000000, 106.484000000000]) + 5
+# offset = [x+5 for x in offset]
 
 i = np.argsort(SBtransform[0])
 test0 = SBtransform[0][i]
 test1 = SBtransform[1][i]
-SBtransformSort = np.array(test0,test1)
+SBtransformSort = np.array(test0, test1)
 
 # As of now, you have to run the program with the CSV file open :-/
 unpackingLenTen = []
@@ -99,30 +108,30 @@ with open('hex_pack_len_ten_prestress.csv', 'rb') as csvfile:
 
     print('Reay...\n')
 
-steps = [len(unpackingLenTen)-1]+range(len(unpackingLenTen)-2,100,-1)
+steps = [len(unpackingLenTen) - 1] + range(len(unpackingLenTen) - 2, 100, -1)
 for j in steps:
     current_lengths = unpackingLenTen[j][0:24]
     current_tensions = unpackingLenTen[j][24:]
 
-    current_tensions = [400.0 if i>400.0 else i for i in current_tensions]
-    current_moments = current_tensions*0.006
+    current_tensions = [400.0 if i > 400.0 else i for i in current_tensions]
+    current_moments = current_tensions * 0.006
 
-    newRestLengths = np.array([current_tensions[i-1] for i in SBtransformSort[1]])
-    newMoments = np.array([current_moments[i-1] for i in SBtransformSort[1]])
+    newRestLengths = np.array([current_tensions[i - 1] for i in SBtransformSort[1]])
+    newMoments = np.array([current_moments[i - 1] for i in SBtransformSort[1]])
 
-    cmdMotorPositions = (100*newRestLengths)*slope+offset
+    cmdMotorPositions = (100 * newRestLengths) * slope + offset
 
     CmdSB.position = cmdMotorPositions
-    CmdSB.effort = newMoments*0.2
+    CmdSB.effort = newMoments * 0.2
 
     GroupSB.send_command(CmdSB)
     sleep(0.025)
 
 print('DOWN')
 
-system("pause") #may function as intended
+system("pause")  # may function as intended
 
-CmdSB.position = np.ones(GroupSB.size)*0.0
+CmdSB.position = np.ones(GroupSB.size) * 0.0
 GroupSB.send_command(CmdSB)
 
 sys.exit()
