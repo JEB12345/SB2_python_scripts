c1b014593db439a812a8ea6647830b7f24a9cf59
Jonathan <jonathan.bruce@nasa.gov>
2018-06-29 13:29:13 -0700

    Removed testing code on line 128

--- Demo_Star_Packing.py
+++ Demo_Star_Packing.py
@@ -125,7 +125,7 @@ for j in steps:
     newRestLengths = np.array([current_tensions[i - 1] for i in SBtransformSort[1]])
     newMoments = np.array([current_moments[i - 1] for i in SBtransformSort[1]])
 
-    cmdMotorPositions = (100 * newRestLengths) * slope[0:2] + offset[0:2]
+    cmdMotorPositions = (100 * newRestLengths) * slope + offset
     print(cmdMotorPositions)
 
     CmdSB.position = cmdMotorPositions
